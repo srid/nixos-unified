26ecf63837522c6a3f92dd91c41b6e0d8b145836
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-06-11 15:49:38 -0400

    path

--- flake-module.nix
+++ flake-module.nix
@@ -66,7 +66,7 @@ in
                           set -x
                           ${emptyConfiguration.system}/sw/bin/darwin-rebuild \
                             switch \
-                            --flake "${self}#''${HOSTNAME}" \
+                            --flake "path:${self}#''${HOSTNAME}" \
                             "$@"
                         ''
                       else
@@ -75,7 +75,7 @@ in
                           set -x
                           ${lib.getExe pkgs.nixos-rebuild} \
                             switch \
-                            --flake "${self}#''${HOSTNAME}" \
+                            --flake "path:${self}#''${HOSTNAME}" \
                             --use-remote-sudo \
                             "$@"
                         '';
