dfd0be20c9eea0f82ff745449daa7fe31591e6f5
Sridhar Ratnakumar <srid@srid.ca>
2024-06-26 16:48:25 -0400

    refactor: scope

--- activate/default.nix
+++ activate/default.nix
@@ -1,31 +1,34 @@
 { self, inputs', pkgs, lib, system, ... }:
 
 let
-  # Workaround https://github.com/NixOS/nix/issues/8752
-  cleanFlake = lib.cleanSourceWith {
-    name = "nixos-flake-activate-flake";
-    src = self;
-  };
-  nixos-flake-configs = lib.mapAttrs (name: value: value.config.nixos-flake) (self.nixosConfigurations or { } // self.darwinConfigurations or { });
-  data = {
-    nixos-flake-configs = nixos-flake-configs;
-    system = system;
-    cleanFlake = cleanFlake;
-  };
-  dataFile = pkgs.writeTextFile {
-    name = "nixos-flake-activate-data";
-    text = ''
-      ${builtins.toJSON data}
-    '';
-  };
-  nixosFlakeNuModule = pkgs.writeTextFile {
-    name = "nixos-flake.nu";
-    text = ''
-      export def getData [] {
-        open ${dataFile} | from json
-      }
-    '';
-  };
+  nixosFlakeNuModule =
+    let
+      # Workaround https://github.com/NixOS/nix/issues/8752
+      cleanFlake = lib.cleanSourceWith {
+        name = "nixos-flake-activate-flake";
+        src = self;
+      };
+      nixos-flake-configs = lib.mapAttrs (name: value: value.config.nixos-flake) (self.nixosConfigurations or { } // self.darwinConfigurations or { });
+      data = {
+        nixos-flake-configs = nixos-flake-configs;
+        system = system;
+        cleanFlake = cleanFlake;
+      };
+      dataFile = pkgs.writeTextFile {
+        name = "nixos-flake-activate-data";
+        text = ''
+          ${builtins.toJSON data}
+        '';
+      };
+    in
+    pkgs.writeTextFile {
+      name = "nixos-flake.nu";
+      text = ''
+        export def getData [] {
+          open ${dataFile} | from json
+        }
+      '';
+    };
   nu = import ../nix/nu.nix { inherit pkgs; };
 in
 nu.writeNushellApplication {
